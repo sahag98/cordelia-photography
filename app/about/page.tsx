ad353c17f10d2e3f576edd4b4b0532bbc5c80d0a
Sahak Arzoumanian <arzsahag@gmail.com>
2023-12-17 21:19:15 -0800

    added static metadata to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "about",
+  description: "Hi, My name is Cordelia!",
+};
 
 const Page = () => {
   return (
